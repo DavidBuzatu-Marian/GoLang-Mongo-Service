7d1bb326ac480e438fc47ff9b9c5468a44346644
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-07-28 20:18:31 +0100

    Refactored methods of person, connect and removed config

--- go_mongo.go
+++ go_mongo.go
@@ -2,10 +2,7 @@ package go_mongo
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -17,26 +14,10 @@ type Config struct {
 	MongoURI string
 }
 
-var config Config
-var client *mongo.Client
 var ctx context.Context
 
-func ReadConfig() {
-	file, _ := os.Open("./config/default.json")
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	config = Config{}
-	err := decoder.Decode(&config)
-	if err != nil {
-		fmt.Println("Error reading config file!")
-		os.Exit(1)
-	}
-}
-
-func ConnectToMongo() {
-	ReadConfig()
-	var err error
-	client, err = mongo.NewClient(options.Client().ApplyURI(config.MongoURI))
+func ConnectToMongo(mongoUri string) *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoUri))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,18 +26,38 @@ func ConnectToMongo() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return client
+}
 
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+func CollectBirthdays(client *mongo.Client) []bson.D {
+	collection := client.Database("myFirstDatabase").Collection("people")
+	query, err := getBirthdaysFromCollection(collection)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(databases)
+	defer query.Close(ctx)
+	people := getPeopleFromQuery(query)
+	return people
 }
 
-func CollectBirthdays() []bson.D {
+func getPeopleFromQuery(query *mongo.Cursor) []bson.D {
 	var people []bson.D
-	collection := client.Database("myFirstDatabase").Collection("people")
-	query, err := collection.Aggregate(ctx, bson.A{
+	for query.Next(ctx) {
+		var result bson.D
+		err := query.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		people = append(people, result)
+	}
+	if err := query.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return people
+}
+
+func getBirthdaysFromCollection(collection *mongo.Collection) (*mongo.Cursor, error) {
+	return collection.Aggregate(ctx, bson.A{
 		bson.M{
 			"$redact": bson.M{
 				"$cond": bson.A{
@@ -74,20 +75,4 @@ func CollectBirthdays() []bson.D {
 							}}},
 					"$$KEEP",
 					"$$PRUNE"}}}})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer query.Close(ctx)
-	for query.Next(ctx) {
-		var result bson.D
-		err := query.Decode(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
-		people = append(people, result)
-	}
-	if err := query.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return people
 }
